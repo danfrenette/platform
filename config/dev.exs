c6e7d81a5cb171920f9187a1bf90c080815de7b6
danfrenette <dan.r.frenette@gmail.com>
2018-01-14 11:05:46 -0500

    Set up database configuration

--- config/dev.exs
+++ config/dev.exs
@@ -51,7 +51,7 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :platform, Platform.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
+  username: "danfrenette",
   password: "postgres",
   database: "platform_dev",
   hostname: "localhost",
