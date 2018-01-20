c1699f9bda9967be25f12dfd46b7e9ef7c885c85
danfrenette <dan.r.frenette@gmail.com>
2018-01-20 12:55:09 -0500

    Restrict access to the app until players are signed in

--- test/platform_web/controllers/player_controller_test.exs
+++ test/platform_web/controllers/player_controller_test.exs
@@ -3,8 +3,10 @@ defmodule PlatformWeb.PlayerControllerTest do
 
   alias Platform.Accounts
 
-  @create_attrs %{score: 42, username: "some username"}
-  @update_attrs %{score: 43, username: "some updated username"}
+
+  @create_attrs %{password: "some password", username: "some username"}
+  @update_attrs %{display_name: "some updated dn", password: "some updated pw",
+    username: "some updated username"}
   @invalid_attrs %{score: nil, username: nil}
 
   def fixture(:player) do
