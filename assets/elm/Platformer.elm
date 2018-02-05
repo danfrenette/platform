1c9be5d4e738c1fc6dc377e3a4f89e19804a97d8
danfrenette <dan.r.frenette@gmail.com>
2018-02-04 20:48:50 -0500

    Create placeholder Elm app for the platformer game
    From page 207 of "The Elixir Elm Tutorial":
    
    We have all the things we need to get started:
    • minimal imports.
    • a main function to wire everything together.
    • an empty Model type and init function.
    • a default update function with a “NoOp” placeholder case that allows us to perform no
    operation. This may seem confusing at first, but it’s helpful to have a default message to
    work with before we start updating our model.
    • an intitial subscriptions function that starts with Sub.none.
    • a simple view with a container div and some text that we can see to ensure that our application is working.

--- assets/elm/Platformer.elm
+++ assets/elm/Platformer.elm
@@ -3,6 +3,62 @@ module Platformer exposing (..)
 import Html exposing (..)
 
 
-main : Html msg
+main : Program Never Model Msg
 main =
-    text "Platformer Game"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+initialModel : Model
+initialModel =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text "Elm Game Base" ]
